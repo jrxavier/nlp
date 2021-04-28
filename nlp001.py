9d21830bbaf8d0a74a7e7f8cd4866823eddce5a0
José Ricardo Xavier <jrxavier@gmail.com>
2021-04-28 16:04:42 -0300

    Insercao de TextBlob

--- nlp001.py
+++ nlp001.py
@@ -1,8 +1,9 @@
+from textblob import TextBlob
 from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 from nltk import word_tokenize
 import nltk
-# nltk.download('punkt')
+nltk.download('punkt')
 
 
 text_pt = "Quem poderia imaginar que programas de computador seriam capazes de analisar sentimentos no texto"
@@ -17,7 +18,7 @@ print(tokens)
     Stop words: are mostly connector words that do not contribute much to the meaning of the sentence
     usar: nltk.download('stopwords')
 """
-# nltk.download('stopwords')
+nltk.download('stopwords')
 stopwords_en = nltk.corpus.stopwords.words('english')
 print("Stop words EN")
 print(stopwords_en)
@@ -33,7 +34,7 @@ print(networks)
 """Lemmanization and stemming: tecniques used to reduce words to their root form
 pre-req: ltk.download('wordnet')
 """
-# nltk.download('wordnet')
+nltk.download('wordnet')
 # Uso de Lemmatization
 lemmatizer = WordNetLemmatizer()
 tokens_lem = [lemmatizer.lemmatize(word) for word in tokens]
@@ -53,7 +54,18 @@ print(token_ps)
 pre-req: nltk.download('averaged_perceptron_tagger')
 """
 nltk.download('averaged_perceptron_tagger')
-text2 = "Usain Bolt is the fastest runner in the word"
+#text2 = "Usain Bolt is the fastest runner in the word"
+text2 = "Usain Bolt é o corredor mais rápido do mundo"
 tokens = word_tokenize(text2)
 print("POS tagging")
 print([nltk.pos_tag([token]) for token in tokens])
+
+# Lista exaustiva de NLTK POS tag
+nltk.download('tagsets')
+# nltk.help.upenn_tagset()
+
+"""Textblob: Sentimental Analysis
+"""
+
+eval = TextBlob("I love pizza").sentiment
+print(eval)
